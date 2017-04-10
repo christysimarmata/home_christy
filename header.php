45e7541a1a358c214473731013db90322745c0a0
Christy Simarmata <christysimarmata7@gmail.com>
2017-04-10 21:46:30 +0700

    init

---
+++ header.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+    
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" >
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+    <!-- Custom styles for this template -->
+    <link href="<?php bloginfo('template_directory'); ?> /rev2_style.css" rel="stylesheet">
+    <?php wp_head();?>
+</head>
+<body> 
+    <div class="kontener">
+        <img id="logo" src="<?php bloginfo('template_directory'); ?> /image/websitebaru/Home/logo.png">
+    <div class="main_menu">
+             <a id="home" href="#">HOME</a>
+             <a id="about" href="#">ABOUT</a>
+             <a id="service" href="#">SERVICE</a>
+             <a id="contact" href="#">CONTACT</a>
+    </div>
---
+++ index.php
@@ -0,0 +1,30 @@
+<?php get_header(); ?>
+    <div class="title">
+    <p class="main_title">
+	<a href="<?php bloginfo('wpurl');?>">
+        <?php echo get_bloginfo('name'); ?>
+        </p>
+    <p class="quote_title">
+        <?php echo get_bloginfo('description'); ?>
+		</p>
+    </div>
+    <div class="explore_now">
+        <a href="#">EXPLORE NOW <i class="fa fa-map-marker"></i></a>
+    </div>
+     <div class="social_media">
+        <a href="#"><img src="<?php bloginfo('template_directory');?> /image/websitebaru/Home/facebook.png"></a>  
+         <br/>
+        <a href="#"><img src="<?php bloginfo('template_directory');?> /image/websitebaru/Home/twitter.png"></a>
+         <br/>
+        <a href="#"><img src="<?php bloginfo('template_directory');?> /image/websitebaru/Home/instagram.png"></a>
+    </div>
+    <div class="spoiler">
+        <p>
+            02-07-17
+            <br/>
+            Ini ibaratnya spoiler tentang berita apasih yang ada di clemflosdotcom. Dipanjangpanjangi dulu dikit kan ya.
+        </p>
+    </div>
+    </div>
+</body>    
+</html>    
